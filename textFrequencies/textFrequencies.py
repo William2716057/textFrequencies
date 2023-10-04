d8804cd6f1dbd80915163a473bc8404d7bb560be
William Spriggs <Willspri@gmail.com>
2023-10-04 14:35:35 +1300

    Add project files.

---
+++ textFrequencies/textFrequencies.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+
+#function to create dictionary for word storage
+def stored(text, wordstoCount):
+    #dictionary
+    wordCounts = {word: 0 for word in wordstoCount}
+
+    #split text into words
+    words = text.split()
+
+    #count words
+    for word in words:
+        if word in wordstoCount:
+            wordCounts[word] += 1
+
+    return wordCounts
+
+#function to plot words
+
+def wordplots(wordCounts):
+    #extract words and counts from wordCounts dictionary
+    words = list(wordCounts.keys())
+    counts = list(wordCounts.values())
+
+    plt.figure(figsize=(10, 6))
+    plt.bar(words, counts)
+    plt.xlabel('Words')
+    plt.ylabel('Occurrences')
+    plt.title('Word Occurrences')
+    plt.xticks(rotation=45, fontsize=10)
+
+    plt.show()
+
+inputText = ""
+
+wordstoCount = [""]
